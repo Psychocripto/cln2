5c74bdf55e7c44c790453d14193ba7c9d10536d9
Psychocripto <83782131+Psychocripto@users.noreply.github.com>
2024-12-30 23:07:12 +0530

    Delete sigbed-main-fixed-with-dependencies-for-vercel directory

--- sigbed-main-fixed-with-dependencies-for-vercel/sigbed-main/src/app/team/2022/page.js
+++
@@ -1,8 +0,0 @@
-export default function Team2022() {
-    return (
-        <div>
-            <h1>Team 2022</h1>
-            <p>This is the page for the 2022 team. Add your content here.</p>
-        </div>
-    );
-}
